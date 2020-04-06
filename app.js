a563f1de8b1eced56079380a0827f9ca27917062
tnanhekhan <1631995@gmail.com>
2020-04-06 14:43:21 +0200

    setup project

---
+++ app.js
@@ -0,0 +1,45 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const port = process.env.PORT || 3002;
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .use(logger('dev'))
+    .use(express.json())
+    .use(express.urlencoded({extended: false}))
+    .use(cookieParser())
+    .use(express.static(path.join(__dirname, 'public')))
+    .use('/', indexRouter)
+    .use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+http.createServer(app).listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+});
+
+module.exports = app;
