193c495740ed60fd09a89e45ec868677c658944c
tnanhekhan <1631995@gmail.com>
2020-04-20 01:05:22 +0200

    working map parking spots

--- public/js/app.js
+++ public/js/app.js
@@ -33,7 +33,10 @@ let map = new ol.Map({
         }),
         new ol.layer.Tile({
             source: new ol.source.TileWMS({
-                url: "https://map.data.amsterdam.nl/maps/parkeervakken?REQUEST=GetCapabilities&VERSION=1.1.0&SERVICE=wms"
+                // url: 'https://map.data.amsterdam.nl/maps/parkeervakken?service=WMS&request=GetMap&version=1.1.1&layers=alle_parkeervakken%2Cparkeervakken_label&styles=&format=image%2Fpng&transparent=true&identify=false&onLoading=function(t)%7Bvar%20n%3Dt.sourceTarget%3Breturn%20e.handleLoading(n)%7D&onLoad=function(t)%7Bvar%20n%3Dt.sourceTarget%3Breturn%20e.handleLoaded(n)%7D&srs=EPSG%3A28992&width=480&height=949&bbox=121314.31038026694,485028.8119350978,121515.9456401163,485427.7690752142',
+                url: 'https://map.data.amsterdam.nl/maps/parkeervakken?service=WMS&request=GetMap&version=1.1.1',
+                params: {"layers": "alle_parkeervakken,parkeervakken_label,parkeervakken_bord"},
+                crossOrigin: "anonymous"
             })
         })
     ],
@@ -49,11 +52,22 @@ let geoCoder = new Geocoder('nominatim', {
     lang: 'nl-NL',
     placeholder: 'Toets hier uw locatie in:',
     targetType: 'glass-button',
-    limit: 8,
+    limit: 10,
     keepOpen: true
 });
 
 map.addControl(geoCoder);
+
+// Get Lat Long from click
+// map.on('click', evt => console.log(evt.coordinate))
+map.on('click', (evt) =>{
+    // https://api.data.amsterdam.nl endpoint
+    //https://api.data.amsterdam.nl/parkeervakken/geosearch/?lat=52.3458067&lon=4.9030355&item=parkeervak api for geoloc
+    const coords = ol.proj.toLonLat(evt.coordinate);
+    const lat = coords[1];
+    const lng = coords[0];
+    console.log(`lat: ${lat} lng: ${lng}`)
+});
 
 // geocoding callback
 geoCoder.on('addresschosen', evt => {
