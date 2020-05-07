43c7a61acc3d00816d15aa9a4418ab39b642577a
tnanhekhan <1631995@gmail.com>
2020-05-07 12:33:51 +0200

    fix white screen

--- public/js/app.js
+++ public/js/app.js
@@ -67,8 +67,7 @@ let wmsSource = new ol.layer.Tile({
         // url: 'https://map.data.amsterdam.nl/maps/parkeervakken?service=WMS&request=GetMap&version=1.1.1&layers=alle_parkeervakken%2Cparkeervakken_label&styles=&format=image%2Fpng&transparent=true&identify=false&onLoading=function(t)%7Bvar%20n%3Dt.sourceTarget%3Breturn%20e.handleLoading(n)%7D&onLoad=function(t)%7Bvar%20n%3Dt.sourceTarget%3Breturn%20e.handleLoaded(n)%7D&srs=EPSG%3A28992&width=480&height=949&bbox=121314.31038026694,485028.8119350978,121515.9456401163,485427.7690752142',
         url: 'https://map.data.amsterdam.nl/maps/parkeervakken?service=WMS&request=GetMap&version=1.1.1',
         params: {"layers": "alle_parkeervakken"},
-        crossOrigin: "anonymous",
-        serverType: "geoServer"
+        crossOrigin: "anonymous"
     })
 });
 
